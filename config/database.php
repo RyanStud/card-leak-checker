970c3906368514fbdfad0006ae459b7a31870682
Rocklee <106843319+EduZaruvnex@users.noreply.github.com>
2026-03-29 22:19:33 -0300

    Setup to local test with XAMP
    Created a environment code to setup the site on localhost, to do security tests before up to main or make merge.

--- config/database.php
+++ config/database.php
@@ -4,7 +4,8 @@ return [
     'host' => env('DB_HOST', '127.0.0.1'),
     'port' => env('DB_PORT', '3306'),
     'dbname' => env('DB_NAME', 'card_leak_checker'),
-    'username' => required_secret('DB_USER'),
-    'password' => required_secret('DB_PASS'),
+    'username' => secret('DB_USER', env('DB_USER', 'root')),
+    'password' => secret('DB_PASS', env('DB_PASS', '')),
+
     'charset' => 'utf8mb4',
 ];
